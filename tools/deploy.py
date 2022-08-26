bc308cd5037975955418b20c93c4fbd78dd52440
Nabil Lahssini <nabillahssini@gmail.com>
2022-08-26 13:01:10 +0200

    Added support to all platforms

--- tools/deploy.py
+++ tools/deploy.py
@@ -4,7 +4,6 @@ import sys
 import hashlib
 import requests
 
-
 PUBLIC_GPG = os.environ.get("GPG_PUBLIC_KEY").replace("\n", "\\n")
 PRIVATE_GPG = os.environ.get("GPG_PRIVATE_KEY").replace("\n", "\\n")
 TF_TOKEN = os.environ.get("TF_TOKEN")
@@ -13,26 +12,42 @@ BASE_URL = "https://app.terraform.io/api"
 WORKSPACE = "reprise-digital"
 PROVIDER = "looker"
 
+def get_sha256(path: str) -> str:
+    with open(path,"rb") as f:
+        bytes = f.read()
+        readable_hash = hashlib.sha256(bytes).hexdigest()
+        return readable_hash
+
+class File:
+    def __init__(self, name: str, version: str= None, platform: str= None, arch: str= None, upload_link: str = None) -> None:
+        self.name = name
+        self.version = version
+        self.platform = platform
+        self.arch = arch
+        self.upload_link = upload_link
+        self.path = os.path.abspath(f"dist/{name}")
+        self.sha256 = get_sha256(self.path)
+
 def get_headers() -> dict:
     return {
         "Authorization": f"Bearer {TF_TOKEN}",
         "Content-Type": "application/vnd.api+json"
     }
 
-def add_platform_endpoint(filename: str, shasum: str, version: str) -> str:
+def add_platform_endpoint(file: File) -> str:
     headers = get_headers()
     payload = {
         "data": {
             "type": "registry-provider-version-platforms",
             "attributes": {
-            "os": "linux",
-            "arch": "amd64",
-            "shasum": shasum,
-            "filename": filename
+            "os": file.platform,
+            "arch": file.arch,
+            "shasum": file.sha256,
+            "filename": file.name
             }
         }
     }
-    response = requests.post(BASE_URL+f"/v2/organizations/{WORKSPACE}/registry-providers/private/{WORKSPACE}/{PROVIDER}/versions/{version}/platforms", headers=headers, json = payload)
+    response = requests.post(BASE_URL+f"/v2/organizations/{WORKSPACE}/registry-providers/private/{WORKSPACE}/{PROVIDER}/versions/{file.version}/platforms", headers=headers, json = payload)
     if 200 <= response.status_code < 300:
         result = response.json()
         upload_link = result["data"]["links"]["provider-binary-upload"]
@@ -63,77 +78,36 @@ def add_version_endpoint(version: str) -> tuple:
     print("Error creating version, check if version already exists.")
     sys.exit(1)
 
-def upload_shasums(path_sha: str, path_sha_sig: str, link_sha: str, link_sha_sig: str) -> None:
-    with open(path_sha, "rb") as file:
-        response = requests.put(link_sha, files = {"upload_file": file})
+def upload_file(file: File):
+    with open(file.path, "rb") as file_data:
+        response = requests.put(file.upload_link, files = {"upload_file": file_data})
         if response.status_code >= 300:
             print(response.content)
-            print(f"Error uploading {path_sha} to {link_sha}.")
+            print(f"Error uploading {file.path} to {file.upload_link}.")
             sys.exit(1)
-
-    with open(path_sha_sig, "rb") as file:
-        response = requests.put(link_sha_sig, files = {"upload_file": file})
-        if response.status_code >= 300:
-            print(response.content)
-            print(f"Error uploading {path_sha_sig} to {link_sha_sig}.")
-            sys.exit(1)
-
-def upload_file(file_path: str, link: str):
-    with open(file_path, "rb") as file:
-        response = requests.put(link, files = {"upload_file": file})
-        if response.status_code >= 300:
-            print(response.content)
-            print(f"Error uploading {file_path} to {link}.")
-            sys.exit(1)
-    
-
-def find_index(term: str, string: str) -> tuple:
-    myarray = []
-    for m in re.finditer(term, string):
-        myarray.append(m.start())
-    return myarray[0]+1, myarray[1]
-
-def get_sha256(path: str) -> str:
-    with open(path,"rb") as f:
-        bytes = f.read() # read entire file as bytes
-        readable_hash = hashlib.sha256(bytes).hexdigest()
-        return readable_hash
 
 def main():
-    sig_file = ""
-    sha_file = ""
-    release_file = ""
+    files: list[File] = []
     for file in os.listdir("dist"):
         try:
-            if file[-14:] == "SHA256SUMS.sig":
-                sig_file = file
+            if file[-4:] == ".zip":
+                try:
+                    _, version, platform, arch = file.split("_")
+                    files.append(File(file, version, platform, arch[:-4], None))
+                except:
+                    pass     
+            elif file[-10:] == "SHA256SUMS":
+                sha_file = File(name = file)
+            elif file[-14:] == "SHA256SUMS.sig":
+                shasig_file = File(name = file)
         except IndexError:
             pass
-        try:
-            if file[-10:] == "SHA256SUMS":
-                sha_file = file
-        except IndexError:
-            pass
-        try:
-            if file[-15:] == "linux_amd64.zip":
-                release_file = file
-        except IndexError:
-            pass
-        
-
-    index_start, index_end = find_index("_", sha_file)
-    version = sig_file[index_start:index_end]
-    sha_file = os.path.abspath(f"dist/{sha_file}")
-    sig_file = os.path.abspath(f"dist/{sig_file}")
-    sha_link, sig_link = add_version_endpoint(version)
-    upload_shasums(sha_file, sig_file, sha_link, sig_link)
-    release_file_name = release_file
-    release_file = os.path.abspath(f"dist/{release_file}")
-    release_sha256 = get_sha256(release_file)
-    upload_link = add_platform_endpoint(release_file_name, release_sha256, version)
-    upload_file(release_file, upload_link)
-
-
+    sha_file.upload_link, shasig_file.upload_link = add_version_endpoint(version)
+    upload_file(sha_file) 
+    upload_file(shasig_file)
+    for file in files:
+        file.upload_link = add_platform_endpoint(file)
+        upload_file(file)
 
 if __name__ == "__main__":
     main()

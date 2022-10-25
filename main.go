1dfb40754d91a7941967e982373b1ab0ff768351
Nabil Lahssini <nabillahssini@gmail.com>
2022-10-25 09:47:08 +0200

    removed unnecessary comments

--- main.go
+++ main.go
@@ -6,20 +6,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but it's suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version string = "0.0.3"
+	version string = "1.0.5"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""
@@ -35,7 +25,7 @@ func main() {
 		Debug: debugMode,
 
 		// TODO: update this string with the full name of your provider as used in your configs
-		ProviderAddr: "app.terraform.io/reprise-digital/looker",
+		ProviderAddr: "app.terraform.io/devoteamgcloud/looker",
 
 		ProviderFunc: provider.New(version),
 	});

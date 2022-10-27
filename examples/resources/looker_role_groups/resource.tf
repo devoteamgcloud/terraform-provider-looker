3e2cab8f11bcbe3acd7483919c8a47a52e61817a
Nabil Lahssini <nabillahssini@gmail.com>
2022-10-27 13:27:54 +0200

    role_member becomes role_groups

--- examples/resources/looker_role_groups/resource.tf
+++ examples/resources/looker_role_groups/resource.tf
@@ -1,5 +1,5 @@
-resource "looker_role_member" "role_member" {
-  target_role_id = "345"
+resource "looker_role_groups" "role_member" {
+  role_id = "345"
   group {
     id = looker_group.group_admin.id
   }

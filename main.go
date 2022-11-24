f16cfe805cf49d653cb50bbd8e4be7662439f3cf
Nabil Lahssini <nabillahssini@gmail.com>
2022-11-24 15:02:14 +0100

    0.2.0 version release

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version string = "0.1.2"
+	version string = "0.2.0"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""

82da7d9a9f7de094e221df765f5882a86d6a87c2
gwiazdp <patryk.gwiazda@admiralgroup.co.uk>
2022-12-05 16:03:38 +0000

    Added example of method

--- examples/resources/looker_project_git_repo/resource.tf
+++ examples/resources/looker_project_git_repo/resource.tf
@@ -2,4 +2,6 @@ resource "looker_project_git_repo" "myrepo-project" {
   project_id       = "project-id"
   git_remote_url   = "git@github.com:workspace/repo.git"
   git_service_name = "github"
+  git_username     = "username"
+  git_password     = "password"
 }

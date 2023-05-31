0fe39c6b0bd12348d404cbe3be07944cd97d6014
Nabil Lahssini <nabillahssini@gmail.com>
2023-05-31 10:16:22 +0200

    Documentation update

--- examples/resources/looker_user/resource.tf
+++ examples/resources/looker_user/resource.tf
@@ -1,5 +1,6 @@
 resource "looker_user" "user_a" {
-  first_name = "Xavier"
-  last_name  = "Waterslaeghers"
-  email      = "xavier.w@ipv4.plus"
+  first_name          = "Xavier"
+  last_name           = "Waterslaeghers"
+  email               = "xavier.w@ipv4.plus"
+  send_password_reset = true
 }

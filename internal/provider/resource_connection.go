9c815e9a5493c6b503759234f6718b5a20d2c84a
Nabil-Lahssini <61270565+Nabil-Lahssini@users.noreply.github.com>
2023-09-27 16:13:21 +0200

    Implement update method for looker connections. (#69)

--- internal/provider/resource_connection.go
+++ internal/provider/resource_connection.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/devoteamgcloud/terraform-provider-looker/pkg/lookergo"
-	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -293,25 +292,150 @@ func resourceConnectionRead(ctx context.Context, d *schema.ResourceData, m inter
 	if err != nil {
 		return diag.FromErr(err)
 	}
+	if err = d.Set("name", connection.Name); err != nil {
+		return diag.FromErr(err)
+	}
+	if err = d.Set("host", connection.Host); err != nil {
+		return diag.FromErr(err)
+	}
 
-	if d.HasChanges("name", "host", "port", "username", "user_attribute_fields") {
-		if d.Set("name", connection.Name) != nil {
-			return diag.FromErr(err)
-		}
-		if d.Set("host", connection.Host) != nil {
-			return diag.FromErr(err)
-		}
-		if d.Set("port", connection.Port) != nil {
-			return diag.FromErr(err)
-		}
-		if d.Set("username", connection.Username) != nil {
-			return diag.FromErr(err)
-		}
-		if d.Set("user_attribute_fields", connection.UserAttributeFields) != nil {
+	if err = d.Set("user_attribute_fields", connection.UserAttributeFields); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("port", connection.Port); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("username", connection.Username); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("password", connection.Password); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("certificate", connection.Certificate); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("file_type", connection.FileType); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("database", connection.Database); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("db_timezone", connection.DbTimezone); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("query_timezone", connection.QueryTimezone); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("schema", connection.Schema); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("max_billing_gigabytes", connection.MaxBillingGigabytes); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("tmp_db_name", connection.TmpDbName); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("jdbc_additional_params", connection.JdbcAdditionalParams); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("dialect_name", connection.DialectName); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("maintenance_cron", connection.MaintenanceCron); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("after_connect_statements", connection.AfterConnectStatements); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("tunnel_id", connection.TunnelId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("oauth_application_id", connection.OauthApplicationId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if connection.Ssl != nil {
+		if err = d.Set("ssl", *connection.Ssl); err != nil {
 			return diag.FromErr(err)
 		}
 	}
 
+	if connection.VerifySsl != nil {
+		if err = d.Set("verify_ssl", *connection.VerifySsl); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.UserDbCredentials != nil {
+		if err = d.Set("user_db_credentials", *connection.UserDbCredentials); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.SqlRunnerPrecacheTables != nil {
+		if err = d.Set("sql_runner_precache_tables", *connection.SqlRunnerPrecacheTables); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.SqlWritingWithInfoSchema != nil {
+		if err = d.Set("sql_writing_with_info_schema", *connection.SqlWritingWithInfoSchema); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.DisableContextComment != nil {
+		if err = d.Set("disable_context_comment", *connection.DisableContextComment); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.AlwaysRetryFailedBuilds != nil {
+		if err = d.Set("always_retry_failed_builds", *connection.AlwaysRetryFailedBuilds); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.CostEstimateEnabled != nil {
+		if err = d.Set("cost_estimate_enabled", *connection.CostEstimateEnabled); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if connection.PdtApiControlEnabled != nil {
+		if err = d.Set("pdt_api_control_enabled", *connection.PdtApiControlEnabled); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if err = d.Set("max_connections", connection.MaxConnections); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("pool_timeout", connection.PoolTimeout); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = d.Set("pdt_concurrency", connection.PdtConcurrency); err != nil {
+		return diag.FromErr(err)
+	}
 	tflog.Trace(ctx, fmt.Sprintf("Fn: %v, Action: end", currFuncName()))
 	return diags
 }
@@ -321,19 +445,108 @@ func resourceConnectionUpdate(ctx context.Context, d *schema.ResourceData, m int
 	tflog.Trace(ctx, fmt.Sprintf("Fn: %v, Action: start", currFuncName()))
 
 	connectionName := d.Get("name").(string)
-	_, _, err := c.Connections.Get(ctx, connectionName)
+	connection, _, err := c.Connections.Get(ctx, connectionName)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	_ = append(diags, diag.Diagnostic{
-		Severity: diag.Warning,
-		Summary:  "WARNING connection update is not implemented",
-		AttributePath: cty.Path{
-			cty.GetAttrStep{Name: "name"},
-		},
-	})
-
+	if d.HasChange("host") {
+		connection.Host = d.Get("host").(string)
+	}
+	if d.HasChange("user_attribute_fields") {
+		connection.UserAttributeFields = interfaceListToStringList(d.Get("user_attribute_fields").([]interface{}))
+	}
+	if d.HasChange("port") {
+		connection.Port = d.Get("port").(string)
+	}
+	if d.HasChange("username") {
+		connection.Username = d.Get("username").(string)
+	}
+	if d.HasChange("password") {
+		connection.Password = d.Get("password").(string)
+	}
+	if d.HasChange("certificate") {
+		connection.Certificate = d.Get("certificate").(string)
+	}
+	if d.HasChange("file_type") {
+		connection.FileType = d.Get("file_type").(string)
+	}
+	if d.HasChange("database") {
+		connection.Database = d.Get("database").(string)
+	}
+	if d.HasChange("db_timezone") {
+		connection.DbTimezone = d.Get("db_timezone").(string)
+	}
+	if d.HasChange("query_timezone") {
+		connection.QueryTimezone = d.Get("query_timezone").(string)
+	}
+	if d.HasChange("schema") {
+		connection.Schema = d.Get("schema").(string)
+	}
+	if d.HasChange("max_billing_gigabytes") {
+		connection.MaxBillingGigabytes = d.Get("max_billing_gigabytes").(string)
+	}
+	if d.HasChange("tmp_db_name") {
+		connection.TmpDbName = d.Get("tmp_db_name").(string)
+	}
+	if d.HasChange("jdbc_additional_params") {
+		connection.JdbcAdditionalParams = d.Get("jdbc_additional_params").(string)
+	}
+	if d.HasChange("dialect_name") {
+		connection.DialectName = d.Get("dialect_name").(string)
+	}
+	if d.HasChange("maintenance_cron") {
+		connection.MaintenanceCron = d.Get("maintenance_cron").(string)
+	}
+	if d.HasChange("after_connect_statements") {
+		connection.AfterConnectStatements = d.Get("after_connect_statements").(string)
+	}
+	if d.HasChange("tunnel_id") {
+		connection.TunnelId = d.Get("tunnel_id").(string)
+	}
+	if d.HasChange("oauth_application_id") {
+		connection.OauthApplicationId = d.Get("oauth_application_id").(string)
+	}
+	if d.HasChange("ssl") {
+		connection.Ssl = boolPtr(d.Get("ssl").(bool))
+	}
+	if d.HasChange("verify_ssl") {
+		connection.VerifySsl = boolPtr(d.Get("verify_ssl").(bool))
+	}
+	if d.HasChange("user_db_credentials") {
+		connection.UserDbCredentials = boolPtr(d.Get("user_db_credentials").(bool))
+	}
+	if d.HasChange("sql_runner_precache_tables") {
+		connection.SqlRunnerPrecacheTables = boolPtr(d.Get("sql_runner_precache_tables").(bool))
+	}
+	if d.HasChange("sql_writing_with_info_schema") {
+		connection.SqlWritingWithInfoSchema = boolPtr(d.Get("sql_writing_with_info_schema").(bool))
+	}
+	if d.HasChange("disable_context_comment") {
+		connection.DisableContextComment = boolPtr(d.Get("disable_context_comment").(bool))
+	}
+	if d.HasChange("always_retry_failed_builds") {
+		connection.AlwaysRetryFailedBuilds = boolPtr(d.Get("always_retry_failed_builds").(bool))
+	}
+	if d.HasChange("cost_estimate_enabled") {
+		connection.CostEstimateEnabled = boolPtr(d.Get("cost_estimate_enabled").(bool))
+	}
+	if d.HasChange("pdt_api_control_enabled") {
+		connection.PdtApiControlEnabled = boolPtr(d.Get("pdt_api_control_enabled").(bool))
+	}
+	if d.HasChange("max_connections") {
+		connection.MaxConnections = int64(d.Get("max_connections").(int))
+	}
+	if d.HasChange("pool_timeout") {
+		connection.PoolTimeout = int64(d.Get("pool_timeout").(int))
+	}
+	if d.HasChange("pdt_concurrency") {
+		connection.PdtConcurrency = int64(d.Get("pdt_concurrency").(int))
+	}
+	_, _, err = c.Connections.Update(ctx, connectionName, connection)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 	tflog.Trace(ctx, fmt.Sprintf("Fn: %v, Action: end", currFuncName()))
 	return resourceConnectionRead(ctx, d, m)
 }

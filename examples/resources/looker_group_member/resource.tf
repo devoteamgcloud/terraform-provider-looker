89e99bb877f4dcf6553281cee5f7c097988bba48
Nabil Lahssini <nabillahssini@gmail.com>
2023-11-22 14:53:05 +0100

    Fix documentation

--- examples/resources/looker_group_member/resource.tf
+++ examples/resources/looker_group_member/resource.tf
@@ -3,7 +3,7 @@ data "looker_group" "group_two" {
 }
 
 resource "looker_group_member" "member_binding" {
-  group_id = data.looker_group.group_two.id
+  target_group_id = data.looker_group.group_two.id
 
   user {
     id = "1"
@@ -15,7 +15,7 @@ resource "looker_group_member" "member_binding" {
 }
 
 resource "looker_group_member" "member_binding_secundo" {
-  group_id = data.looker_group.group_two.id
+  target_group_id = data.looker_group.group_two.id
 
   user {
     id = "4"

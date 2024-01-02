dbbb04262d84a29c7069adec8053453ac8adea83
da-moon <contact@havi.dev>
2024-01-02 12:35:24 -0500

    docs(pkg/lookergo): add comments after defining the interface which interacts with Content Metadata Access API
    - Add API call URI suffix constant `ContentMetaGroupUserBasePath`.
    - Define `ContentMetaGroupUserResource` interface with documentation.
    - Implement `ListByID` method reference as part of the interface.
    - Expand `ContentMetaGroupUser` struct to include detailed field comments.
    - Ensure the struct now accurately represents Looker content metadata access permissions.
    
    Refs: #57
    Signed-off-by: da-moon <contact@havi.dev>

--- pkg/lookergo/content.go
+++ pkg/lookergo/content.go
@@ -4,16 +4,22 @@ import (
 	"context"
 )
 
+// ContentMetaGroupUserBasePath is the API call URI suffix for content metadata access
 const ContentMetaGroupUserBasePath = "4.0/content_metadata_access"
 
+// ContentMetaGroupUserResource is an interface that defines required methods
+// for interacting with Looker content metadata access,e.g. a folder permission.
 type ContentMetaGroupUserResource interface {
+	// ListByID implements ContentMetaGroupUserResource. it is primarily meant to be used against Looker folders. It lists out all permissions a folder has.
 	ListByID(context.Context, string, *ListOptions) ([]ContentMetaGroupUser, *Response, error)
 }
+
+// ContentMetaGroupUser is a struct that represents a Looker content metadata access,e.g. a folder permission. Reference : https://developers.looker.com/api/explorer/4.0/methods/Content/all_content_metadata_accesses?sdk=go
 type ContentMetaGroupUser struct {
-	Can               map[string]bool `json:"can,omitempty"`
-	Id                string          `json:"id"`
-	ContentMetadataId string          `json:"content_metadata_id"`
-	PermissionType    string          `json:"permission_type"`
-	GroupId           string          `json:"group_id"`
-	UserId            string          `json:"user_id"`
+	Can               map[string]bool `json:"can,omitempty"`       // Operations the current user is able to perform on this object
+	Id                string          `json:"id"`                  // Unique Id
+	ContentMetadataId string          `json:"content_metadata_id"` // ID of associated content metadata
+	PermissionType    string          `json:"permission_type"`     // Type of permission: "view" or "edit" Valid values are: "view", "edit"
+	GroupId           string          `json:"group_id"`            // ID of associated group
+	UserId            string          `json:"user_id"`             // ID of associated user
 }

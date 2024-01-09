a77457f25d01754057167601797d481105f629ca
da-moon <contact@havi.dev>
2024-01-09 13:24:32 -0500

    docs(interna;/testutils): init
    Refs: #57
    Signed-off-by: da-moon <contact@havi.dev>

---
+++ internal/testutils/docs.go
@@ -0,0 +1,6 @@
+// Package testutils provides utilities for testing.
+// the code in this package was either inspired by or copied from the
+// folllowing:
+// - https://github.com/spacelift-io/terraform-provider-spacelift
+// - https://github.com/hashicorp/terraform-provider-vault
+package testutils
